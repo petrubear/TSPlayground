fcfd7ffa939e3b3ca3d26589f18d74d102d1568c
petrubear <edison.martinez@gmail.com>
2023-09-02 14:10:23 -0500

    feat: initial import

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import 'reflect-metadata';
+import {Container, Inject, Service} from "typedi";
+import {InstrumentService} from "./service/InstrumentService";
+
+@Service('index')
+class Index {
+    instrumentService: InstrumentService;
+
+    constructor(@Inject('instrument.service') instrumentService: InstrumentService) {
+        this.instrumentService = instrumentService;
+    }
+
+    randomPractice() {
+        const instrument = this.instrumentService.randomPractice();
+        instrument.then(i => console.log(i));
+    }
+}
+
+function main() {
+    const index = Container.get<Index>('index');
+    index.randomPractice();
+}
+
+main();
---
+++ src/service/InstrumentService.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata';
+import {Inject, Service} from "typedi";
+import {Instrument} from "../model/Instrument";
+import {InstrumentRepository} from "../repository/InstrumentRepository";
+
+@Service('instrument.service')
+export class InstrumentService {
+    instrumentRepository: InstrumentRepository;
+
+    constructor(@Inject('instrument.repository') instrumentRepository: InstrumentRepository) {
+        this.instrumentRepository = instrumentRepository;
+    }
+
+    async randomPractice(): Promise<Instrument> {
+        const instruments = await this.instrumentRepository.findAll();
+        const randomIndex = Math.floor(Math.random() * instruments.length);
+        return Promise.resolve(instruments[randomIndex]);
+    }
+}
